8abd43485937629ceff88a4553b1cc8158be6b24
mishratatai <tataimishra321@gmail.com>
2025-12-22 00:11:58 +0530

    Fix: File name update from user-contexts to user-context.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { UserProvider } from "@/contexts/user-contexts";
+import { UserProvider } from "@/contexts/user-context";
 
 createRoot(document.getElementById("root")).render(
      <StrictMode>
